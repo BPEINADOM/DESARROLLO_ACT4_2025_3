d432c285c7743b39c82afc3d55f5e20f3ad15b29
7502410037BillyPeinadoM <bpeinadom@unicartagena.edu.co>
2025-07-04 13:20:12 -0500

    Capa Aplicacion - Implementacion de los metodos CRUD en la capa Service.

---
+++ ejerciciolavadero/src/main/java/co/edu/udec/lavadero/application/service/ServicioServiceImpl.java
@@ -0,0 +1,46 @@
+package co.edu.udec.lavadero.application.service;
+
+import java.util.List;
+
+import co.edu.udec.lavadero.application.ports.in.ServicioServicePort;
+import co.edu.udec.lavadero.application.ports.out.ServicioPersistencePort;
+import co.edu.udec.lavadero.domain.model.Servicio;
+
+public class ServicioServiceImpl implements ServicioServicePort {
+
+    private final ServicioPersistencePort servicioPersistencePort;
+
+    public ServicioServiceImpl(ServicioPersistencePort servicioPersistencePort) {
+        this.servicioPersistencePort = servicioPersistencePort;
+    }
+
+    @Override
+    public void crear(Servicio servicio) {
+        servicioPersistencePort.guardar(servicio);
+    }
+
+
+    @Override
+    public List<Servicio> listarTodos() {
+        return servicioPersistencePort.buscarTodos();
+    }
+
+    @Override
+    public Servicio obtenerPorID(int servicio_id) {
+        return servicioPersistencePort.buscarPorID(servicio_id)
+        .orElseThrow(() -> new RuntimeException("Servicio con ID " + servicio_id + " no encontrado."));
+    }
+
+    @Override
+    public void actualizar(Servicio servicio) {
+        if (!servicioPersistencePort.buscarPorID(servicio.getServicio_id()).isPresent()) {
+            throw new RuntimeException("Servicio con ID" + servicio.getServicio_id() + "no encontrado para actualizar.");
+        }
+    }
+
+    @Override
+    public void borrar(int servicio_id) {
+        servicioPersistencePort.borrar(servicio_id);
+    }
+    
+}
